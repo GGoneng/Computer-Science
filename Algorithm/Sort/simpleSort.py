4da6c72dce5adee2c45619d3d3383d612aa4793a
GGoneng <zxzx0031@naver.com>
2026-01-01 23:09:13 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Algorithm/Sort/simpleSort.py

--- Algorithm/Sort/simpleSort.py
+++ Algorithm/Sort/simpleSort.py
@@ -9,6 +9,16 @@ def selection_sort(A):
 
         print(f"Step {i + 1:2d} = {A}")
 
+def insertion_sort(A):
+    n = len(A)
+    for i in range(1, n):
+        key = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > key:
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = key
+
 if __name__ == "__main__":
     data = [6, 3, 7, 4, 9, 1, 5, 2, 8]
     print("Original : ", data)
