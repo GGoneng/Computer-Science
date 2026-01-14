50dcbcc77bdbddb1b68bee0a77ece6fc4877197f
GGoneng <zxzx0031@naver.com>
2026-01-14 23:43:01 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Algorithm/Search/BinSrchTree.py
    	modified:   Algorithm/Search/simpleSearch.py

--- Algorithm/Search/BinSrchTree.py
+++ Algorithm/Search/BinSrchTree.py
@@ -1,10 +1,12 @@
+# 이진 탐색 트리를 위한 이진 트리
 class BSTNode:
     def __init__(self, key, value):
-        self.key = key
-        self.value = value
+        self.key = key 
+        self.value = value # Value 값 추가
         self.left = None
         self.right = None
 
+# 이진 탐색 트리의 탐색 연산
 def search_bst(n, key):
     if n == None:
         return None
@@ -14,7 +16,8 @@ def search_bst(n, key):
         return search_bst(n.left, key)
     else:
         return search_bst(n.right, key)
-    
+
+# 이진 탐색 트리의 탐색 연산 (Value 값 비교, 전위순회)   
 def search_value_bst(n, value):
     if n == None:
         return None
@@ -26,3 +29,22 @@ def search_value_bst(n, value):
         return res
     else:
         return search_value_bst(n.right, value)
+
+# 이진 탐색 트리의 삽입 연산
+def insert_bst(root, node):
+    if root == None: # root가 공백이라면, 이 위치에 삽입
+        return node
+    
+    if node.key == root.key: # root와 node의 key 값이 같다면, 중복 key 값을 방지하기 위하여 삽입 X
+        return root
+
+    # node의 key 값이 root의 key 값 보다 작을 시 root의 left에 삽입 연산을 순환 호출    
+    if node.key < root.key:
+        root.left = insert_bst(root.left, node)
+
+    # node의 key 값이 root의 key 값 보다 클 시 root의 right에 삽입 연산을 순환 호출        
+    else:
+        root.right = insert_bst(root.right, node)
+
+    return root
+    
--- Algorithm/Search/simpleSearch.py
+++ Algorithm/Search/simpleSearch.py
@@ -1,9 +1,11 @@
+# 순차 탐색
 def sequential_search(A, key, low, high):
     for i in range(low, high + 1):
         if A[i] == key:
             return i
     return -1
 
+# 순차 탐색 (교환하기) : 자주 탐색되는 Key를 앞 쪽으로 배치, 시간이 지나면 자연스레 자주 탐색되는 Key 위주로 앞 쪽에 배치
 def sequential_search_transpose(A, key, low, high):
     for i in range(low, high + 1):
         if A[i] == key:
@@ -13,6 +15,7 @@ def sequential_search_transpose(A, key, low, high):
             return i
     return -1
 
+# 이진 탐색 (순환 구조)
 def binary_search(A, key, low, high):
     if (low <= high):
         middle = (low + high) // 2
@@ -25,6 +28,7 @@ def binary_search(A, key, low, high):
     
     return -1
 
+# 이진 탐색 (순환 구조 X, 반복문 구조)
 def binary_search_iter(A, key, low, high):
     while (low <= high):
         middle = (low + high) // 2
@@ -37,6 +41,7 @@ def binary_search_iter(A, key, low, high):
     
     return -1
 
+# 보간 탐색
 def interpolation_search(A, key, low, high):
     if (low <= high):
         middle = int(low + (high - low) * (key - A[low].key) / (A[high].key - A[low].key))
